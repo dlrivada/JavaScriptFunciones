f2b5f494066707c6c4eb25f8dfcdcf136a26dde3
David Lozano Rivada <dlrivada@hotmail.com>
2015-09-29 10:31:00 +0200

    29092015

---
+++ JavaScriptFunciones/demofunciones.js
@@ -0,0 +1,26 @@
+﻿var saludo = function (e) {
+    alert("1. Hola " + e.button.toString());
+}
+
+function saludando(e) {
+    alert("2. Hola " + e);
+}
+
+//Función autoinvocada
+(function () {
+
+    //Función anónima
+    document.getElementById("btn1").onclick = function (e) {
+        alert("1. Hola " + e.button.toString());
+    };
+
+    //Función variable
+    document.getElementById("btn2").onclick = saludando;
+
+    //Autoinvocación de funciones
+    var x = (function () { return (5 + 6) })();
+    document.writeln('<br />' + typeof (x) + '<br />');
+    document.writeln(x);
+
+})();
+
